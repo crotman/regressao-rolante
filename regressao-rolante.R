d8d631b692697cbe599c0340d8366092b4c6b93d
crotman <crotman@gmail.com>
2023-06-15 06:24:42 -0300

    regressao rolante feita

---
+++ regressao-rolante.R
@@ -0,0 +1,55 @@
+
+
+library(tidyverse)
+library(janitor)
+
+
+roda_regressao = function(bog_rf, ibov_rf, smb, hml, wml){
+
+  dados <- tibble(
+    bog_rf = bog_rf,
+    ibov_rf = ibov_rf,
+    smb = smb,
+    hml = hml,
+    wml = wml
+  )
+
+  modelo = lm(formula = bog_rf ~ ibov_rf + smb + hml + wml, data = dados)
+
+  broom::tidy(modelo) %>%
+    select(
+      term,
+      estimate
+    ) %>%
+    pivot_wider(
+      names_from = term,
+      values_from = estimate
+    )
+
+
+}
+
+dados <- read_csv2("dados.txt") %>%
+  clean_names() %>%
+  mutate(
+    data = make_date(ano, mes, 1)
+  )
+
+
+results = slider::pslide_dfr(
+  .l = list(dados$bog_rf, dados$ibov_rf, dados$smb, dados$hml, dados$wml),
+  .f = roda_regressao,
+  .before = 12
+)
+
+
+coeficientes <- dados %>%
+  select(
+    data
+  ) %>%
+  bind_cols(
+    results
+  )
+
+
+print(coeficientes)
